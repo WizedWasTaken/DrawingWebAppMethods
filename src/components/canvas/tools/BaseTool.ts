f5f2c3c1d28bcdc5abd62c06f2e99d508b44c92a
Noah Nielsen <noahanielsen@gmail.com>
2024-12-05 12:59:28 +0100

    add

--- src/components/canvas/tools/BaseTool.ts
+++ src/components/canvas/tools/BaseTool.ts
@@ -1,23 +1,22 @@
 export default abstract class BaseTool {
-    protected color: string;
-    protected brushSize: number;
-    protected lastPos: { x: number, y: number } | undefined = undefined;
-  
-    constructor(color: string, brushSize: number) {
-      this.color = color;
-      this.brushSize = brushSize;
-    }
-  
-    abstract draw(ctx: CanvasRenderingContext2D, x: number, y: number): void;
-  
-    setColor(color: string) {
-      this.color = color;
-    }
-  
-    setBrushSize(size: number) {
-      this.brushSize = size;
-    }
+  protected color: string;
+  protected brushSize: number;
+  protected lastPos: { x: number; y: number } | undefined = undefined;
 
-    reset() {
-    }
+  constructor(color: string, brushSize: number) {
+    this.color = color;
+    this.brushSize = brushSize;
   }
+
+  abstract draw(ctx: CanvasRenderingContext2D, x: number, y: number): void;
+
+  setColor(color: string) {
+    this.color = color;
+  }
+
+  setBrushSize(size: number) {
+    this.brushSize = size;
+  }
+
+  reset() {}
+}
